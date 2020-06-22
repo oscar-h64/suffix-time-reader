b24de89c62fc89aa5ea6af45201a2f22bccdfb5c
Oscar Harris <oscar@oscar-h.com>
2020-06-22 21:10:47 +0100

    Create module and add ReadAbsoluteTime class

---
+++ src/Data/Time/AbsoluteRead.hs
@@ -0,0 +1,16 @@
+--------------------------------------------------------------------------------
+-- Absolute Time Reader Library                                               --
+--------------------------------------------------------------------------------
+-- This source code is licensed under the BSD3 licence found in the LICENSE   --
+-- file in the root directory of this source tree.                            --
+--                                                                            --
+-- Copyright 2020 Oscar Harris (oscar@oscar-h.com)                            --
+--------------------------------------------------------------------------------
+module Data.Time.AbsoluteRead (
+    ReadAbsoluteTime,
+    readAbsoluteTime
+) where
+
+-- An instance of this typeclass can be read
+class ReadAbsoluteTime a where
+    readAbsoluteTime :: String -> a
