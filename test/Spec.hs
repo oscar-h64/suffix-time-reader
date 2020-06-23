72b324e0ce0273485cbd81aa03323234b7ffdbab
Oscar Harris <oscar@oscar-h.com>
2020-06-23 11:27:05 +0100

    Add tests for `DIffTime`

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,6 +8,7 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import Data.Time.Clock
 import Data.Time.SuffixRead
 
 import Test.Tasty
@@ -18,14 +19,22 @@ tests = localOption (Timeout (5*1000000) "5s") $ testGroup "Data.Time.SuffixRead
     [
         testProperty "returns Nothing on invalid input" $ 
             \x -> (readSuffixTime x :: Maybe Int) === Nothing
-    ,   testProperty "reads seconds correctly" $ 
+    ,   testProperty "reads seconds to Int correctly" $ 
             \(x :: Int) -> (readSuffixTime (show x ++ "s") :: Maybe Int) === Just x
-    ,   testProperty "reads minutes correctly" $ 
+    ,   testProperty "reads minutes to Int correctly" $ 
             \(x :: Int) -> (readSuffixTime (show x ++ "m") :: Maybe Int) === Just (x * 60)
-    ,   testProperty "reads hours correctly" $ 
+    ,   testProperty "reads hours to Int correctly" $ 
             \(x :: Int) -> (readSuffixTime (show x ++ "h") :: Maybe Int) === Just (x * 3600)
-    ,   testProperty "reads days correctly" $ 
+    ,   testProperty "reads days to Int correctly" $ 
             \(x :: Int) -> (readSuffixTime (show x ++ "d") :: Maybe Int) === Just (x * 86400)
+    ,   testProperty "reads seconds to DiffTime correctly" $ 
+            \(x :: Integer) -> (readSuffixTime (show x ++ "s") :: Maybe DiffTime) === Just (secondsToDiffTime x)
+    ,   testProperty "reads minutes to DiffTime correctly" $ 
+            \(x :: Integer) -> (readSuffixTime (show x ++ "m") :: Maybe DiffTime) === Just (secondsToDiffTime $ x * 60)
+    ,   testProperty "reads hours to DiffTime correctly" $ 
+            \(x :: Integer) -> (readSuffixTime (show x ++ "h") :: Maybe DiffTime) === Just (secondsToDiffTime $ x * 3600)
+    ,   testProperty "reads days to DiffTime correctly" $ 
+            \(x :: Integer) -> (readSuffixTime (show x ++ "d") :: Maybe DiffTime) === Just (secondsToDiffTime $ x * 86400)
     ]
 
 main :: IO ()
